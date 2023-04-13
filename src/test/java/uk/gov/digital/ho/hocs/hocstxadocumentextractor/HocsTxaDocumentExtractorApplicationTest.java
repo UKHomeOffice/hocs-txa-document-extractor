de443829110afbc793947b133b61bcaedf22d32b
Conal Walster <conalwalster@hod149783.local>
2023-04-13 12:35:47 +0100

    Add basic sanity check test of application

---
+++ src/test/java/uk/gov/digital/ho/hocs/hocstxadocumentextractor/HocsTxaDocumentExtractorApplicationTest.java
@@ -0,0 +1,17 @@
+package uk.gov.digital.ho.hocs.hocstxadocumentextractor;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class HocsTxaDocumentExtractorApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
